3b577f2fa6b7047f70182c9ccf0086147b469e02
LianaGib <143552368+LianaGib@users.noreply.github.com>
2023-12-17 13:07:44 +0300

    lab2

---
+++ app_weather/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from weather_api import current_weather
+from django.http import JsonResponse
+
+def my_view(request):
+    if request.method == "GET":
+        data = current_weather(59.93, 30.31)
+        return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
+                                                     'indent': 4})
+# Create your views here.
