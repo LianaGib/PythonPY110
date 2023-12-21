09d246e58f5815ab2e494fb2ab8ff5fe3f6e2b15
LianaGib <143552368+LianaGib@users.noreply.github.com>
2023-12-21 22:06:28 +0300

    lab2

--- store/views.py
+++ store/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import DATABASE
 from django.http import HttpResponse, HttpResponseNotFound
+from logic.services import filtering_category
+
 
 def shop_view(request):
     if request.method == "GET":
@@ -9,15 +11,40 @@ def shop_view(request):
             data = f.read()
         return HttpResponse(data)
 
+
 def products_view(request):
     if request.method == "GET":
-        id = request.GET.get('id')
-        if id in DATABASE:
-            return JsonResponse(DATABASE[id], json_dumps_params={'ensure_ascii': False,
+        if id_product := request.GET.get("id"):
+            if data := DATABASE.get(id_product):
+                return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
                                                              'indent': 4})
-        if id not in DATABASE:
             return HttpResponseNotFound("Данного продукта нет в базе данных")
-    if request.method is None:
-        return JsonResponse(DATABASE, json_dumps_params={'ensure_ascii': False,
-                                                         'indent': 4})
+        category_key = request.GET.get("category")
+        if ordering_key := request.GET.get("ordering"):
+            if request.GET.get("reverse") in ('true', 'True'):
+                data = filtering_category(DATABASE, category_key, ordering_key, reverse=True)
+            else:
+                data = filtering_category(DATABASE, category_key, ordering_key, reverse=False)
+        else:
+            data = filtering_category(DATABASE, category_key)
+        return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False,
+                                                                 'indent': 4})
+
+
+def products_page_view(request, page):
+    if request.method == "GET":
+        if isinstance(page, str):
+            for data in DATABASE.values():
+                if data['html'] == page:
+                    with open(f'store/products/{page}.html', 'r', encoding="utf-8") as f:
+                        ex = f.read()
+                    return HttpResponse(ex)
+        elif isinstance(page, int):
+            data = DATABASE.get(str(page))
+            if data:
+                with open(f'store/products/{data["html"]}.html', encoding="utf-8") as f:
+                    ex_ = f.read()
+                    return HttpResponse(ex_)
+            return HttpResponse(status=404)
+
 # Create your views here.
